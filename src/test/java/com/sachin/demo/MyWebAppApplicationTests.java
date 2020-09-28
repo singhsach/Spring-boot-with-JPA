b88141f107aba75f44b454fc88be3ff72652c1db
Sachin <singhsach1312@gmail.com>
2020-09-28 14:37:52 +0530

    Spring Boot JPA

---
+++ src/test/java/com/sachin/demo/MyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
